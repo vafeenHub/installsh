e243f84c51cdb748c461ed87552325ca533bdccc
A <666av6@gmail.com>
2024-06-07 02:14:48 +0300

    07.06.2024 02:14

--- Fedora/new/dnf_install.sh
+++ Fedora/new/dnf_install.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 set -e
 
+# repo for virtualbox
+sudo wget http://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo -P /etc/yum.repos.d/
+
 DNFS=(
    snapd
    git
    g++
    flameshot
+   dconf-editor
+   wine
+   gnome-tweak-tool
+   VirtualBox-7.0
+   playonlinux
+   winetricks
 )
 
 for app in "${DNFS[@]}"
@@ -24,8 +33,10 @@ FLATPAKS=(
 com.github.eneshecan.WhatsAppForLinux
 # Android-Studio
 com.google.AndroidStudio
-# VSCode 
-com.visualstudio.code
+# pea7zip
+io.github.peazip.PeaZip
+# resources 
+net.nokyan.Resources
 ) 
 for fltp in "${FLATPAKS[@]}"
 do
@@ -33,11 +44,27 @@ do
 done
 
 
-SNAPS=(
-p7zip-desktop
+
+SNAPSclassic=(
+    # VSCode 
+    code
+    obsidian
+    intellij-idea-community
+    whatsapp-for-linux
 )
+for snp in "${SNAPSclassic[@]}"
+do
+    sudo snap install "$snp" --classic
+done
+
+SNAPS=(
+    #office
+    onlyoffice-ds
+    )
 for snp in "${SNAPS[@]}"
 do
     sudo snap install "$snp"
 done
 
+sudo dnf upgrade 
+sudo flatpak update
