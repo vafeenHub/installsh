88ddefa8b75ccd2ef31e6b4b04baf96825ed1f97
A <666av6@gmail.com>
2024-11-26 14:05:00 +0300

    26 11 2024 14:05:00

---
+++ repos.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+# repo for virtualbox
+# sudo wget http://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo -P /etc/yum.repos.d/
--- sudo_main.sh
+++ sudo_main.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# repo for virtualbox
-# sudo wget http://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo -P /etc/yum.repos.d/
 
 
 
@@ -20,11 +18,11 @@ sudo dnf remove libreoffice-data
 sudo dnf upgrade
 
 
-sudo flatpak install -y    com.microsoft.Edge
-sudo flatpak install -y   com.github.skylot.jadx
-sudo flatpak install -y   net.nokyan.Resources
-sudo flatpak install -y  org.onlyoffice.desktopeditors
-
+sudo flatpak install -y com.microsoft.Edge
+sudo flatpak install -y com.github.skylot.jadx
+sudo flatpak install -y net.nokyan.Resources
+sudo flatpak install -y org.onlyoffice.desktopeditors
+sudo flatpak install -y com.brave.Browser
 # установка snap
 sudo dnf install -y snapd
 
